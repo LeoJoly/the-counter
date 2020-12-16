ff2c0c8bacf5127e54ef7fa57dbf7b070a2d9a12
LeoJoly <joly.leo@gmail.com>
2020-12-16 16:49:14 +0100

    React model setup

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+// yarn import
+import React from 'react';
+
+// import
+import './styles.scss';
+
+// componant
+const App = () => (
+  <div className="app">
+    <h1>Hi amigo ! I'm your beautiful new app :-)</h1>
+  </div>
+);
+
+// export
+export default App;
